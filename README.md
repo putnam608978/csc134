f6bbacfe079adb14028a8d6464335b0d87eb6e82
putnam608978 <putnam.elijah4031@gmail.com>
2025-08-20 15:43:15 +0000

    My program works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # csc134
 CSC 134 C++ Programming
+
+This is the repository for Elijah
+
+First programs: Hello Worlds.
---
+++ m1t1_hello.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M1T1-Hello World
+E Putnam
+8/20/25
+Frist Program.
+*/
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello, World!" <<endl; // "end-elle"
+    cout << "My foo is C++" <<endl; // put your 'foo' here
+    return 0;
+    
+}
