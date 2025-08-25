e878d782378db6f8fefe6e0c67c2fa87146ab460
putnam608978 <putnam.elijah4031@gmail.com>
2025-08-25 15:04:26 +0000

    M1T1, successfully greets the user

---
+++ m1t1.cpp
@@ -0,0 +1,21 @@
+/*
+m1t1-hello, user
+putnam, 8/25/25
+
+INPUT: run the program
+PROCESS: None yet
+OUTPUT: return 0 (no error)
+SPECIAL FX: Greet the user.
+*/
+
+//magic words to start
+#include <iostream>
+using namespace std;
+
+int main (){
+    //SFX: Greet user
+    cout <<"Hello, user!"<< endl;
+
+    //output
+    return 0; // no errors
+}
