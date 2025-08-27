4ceb27e89972655a685238faa6092e14cf9ab116
putnam608978 <putnam.elijah4031@gmail.com>
2025-08-27 14:39:52 +0000

    Made m1lab

---
+++ .vscode/m1lab.cpp
@@ -0,0 +1,25 @@
+/*
+csc 134
+m1lab
+putnam
+8/27/2025
+purpose: The "gem" (replace noun) sales program.
+*/
+
+//magic words
+#include <iostream>
+using namespace std;
+
+int main(){
+    //strat program
+    //declare all of our variables ("whiteboards")
+    string item = "gem"; // replace with your item
+    int num_items = 10;
+    double cost_per = 0.25;
+
+    cout << "Welcome to the " << item << "store!" << endl;
+    //diuble total_cost =numitem * cost_per;
+    
+    //end program
+    return 0;
+}
