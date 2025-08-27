23490325d76842441e77985f873caaca3d569b8c
putnam608978 <putnam.elijah4031@gmail.com>
2025-08-27 15:03:00 +0000

    added thanks for shoping with us

--- .vscode/m1lab.cpp
+++ .vscode/m1lab.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main(){
     //strat program
     //declare all of our variables ("whiteboards")
-    string item_name = "gem"; // replace with your item
+    string item_name = "Bolter"; // replace with your item
     int num_items = 10 ;
     double cost_per = 0.25;
 
@@ -22,8 +22,10 @@ int main(){
     cout << "Welcome to the " << item_name << " store!" << endl;
     cout << "We have " << num_items << " total." << endl;
     // do the processing
-    //double total_cost =numitem * cost_per;
-
+    double total_cost = num_items * cost_per;
+    // Print the output
+    cout << "Buying them all will cost: $" << total_cost << endl;
+    cout << "Thanks for shopping with us!" << endl;
 
     //end program
     return 0;
