c9e411d4805e1baedd2e6f310e22387aa6308ced
putnam608978 <putnam.elijah4031@gmail.com>
2025-09-14 23:44:38 +0000

    made the code and added movie qoute

---
+++ m1hw1_Putnam.cpp
@@ -0,0 +1,42 @@
+/*
+csc 134
+Putnam, Elijah
+9/14/25
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+
+// declare variables
+string nameofmovie;
+string moviequote;
+int yearofmovie;
+double gross;
+
+//initialize variables
+nameofmovie = "The Lord of the Rings the Fellowship of the Ring.";
+yearofmovie = 2001;
+gross = 887000000.00;
+moviequote = "\"you shall not pass\"";
+
+cout << nameofmovie  << endl;
+cout << yearofmovie  << endl;
+cout <<  gross << endl;
+cout << "This is a great movie it is the first movie in the greatest trilogy ever made." << endl;
+cout << "Tolken the auther was a WW1 veteran and the horrers he saw durring the war helped inspire many aspects of the books" << endl;
+cout << " one of the greatest movie qoutes of all time came from this movie when the charater Gandalf says You shall not pass" << endl;
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
+
+
