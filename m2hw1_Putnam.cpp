7d8ed18a1ee2f072531afd5d1f56a7d623b6068e
putnam608978 <putnam.elijah4031@gmail.com>
2025-09-15 14:57:28 +0000

    worked on debugging not working yet

---
+++ m2hw1_Putnam.cpp
@@ -0,0 +1,38 @@
+/*
+csc134
+M2HW1
+Elijah Putnam
+9/15/25
+*/
+#include <iostream>
+using namespace std;
+
+    // for reasons we'll learn ;ater,
+    // put the other functions above main()
+
+    void question1 () {
+        cout << "Question 1" << endl;
+
+    }
+
+   void question2 () {
+        cout << "Question 2" << endl;
+
+    }
+
+
+int main () {
+        // Call each question as its own function
+
+        question1()
+        question2()
+
+        cout << "Example of printing out correct money values" << endl;
+        double cost = 8.5; // $8.50
+        cout << setprecision(2) << fixed; // requires "#include <iomainip>"
+        cout << "The cost is: $" << cost << endl;
+
+    return 0
+
+}
+
