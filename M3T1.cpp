cec88e9cee07b0ba144d01e2a0885455eb41cdcd
putnam608978 <putnam.elijah4031@gmail.com>
2025-09-17 14:37:34 +0000

    coded for 2 rectangles, and so they could be compared

---
+++ M3T1.cpp
@@ -0,0 +1,46 @@
+/*
+CSC 134
+M3T1 - Area of rectangle
+Simple comparison
+Putnam, Elijah
+9/17/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Get the info of the two rectangles
+    double length1, length2, width1, width2;
+    double area1, area2;
+    cout << "Enter the length and width of two rectangles." << endl;
+    cout << "Frist rectangle." << endl;
+    cout << "length: ";
+    cin  >> length1;
+    cout << "Width: ";
+    cin >> width1;
+    cout << endl;
+    cout << "Second rectanglr." << endl;
+    cout << "Length: ";
+    cin >> length2;
+    cout << "Width: ";
+    cin >> width2;
+
+    // Calculate the area
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
+    // Print the output
+
+
+
+    return 0; // no errors
+
+}
+
+
+
+
+
+
