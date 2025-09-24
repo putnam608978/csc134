c47a13bc0ffd235c786febc9fab7e363ac01ef46
putnam608978 <putnam.elijah4031@gmail.com>
2025-09-24 15:09:31 +0000

    senario 1 both options done

--- m3lab1.cpp
+++ m3lab1.cpp
@@ -10,7 +10,7 @@ Putnam, Elijah
 using namespace std;
 // ========== FUNCTION PROTOTYPES ==========
 // Declare all your "rooms" up here
-void tavern(); // Starting location
+void fighting_postion_Kilo(); // Starting location
 void forest(); // A possible path
 void castle(); // Another path
 void gameOver(); // An ending
@@ -23,27 +23,27 @@ cout << " WELCOME TO YOUR ADVENTURE " << endl;
 cout << "==================================" << endl;
 cout << endl;
 // Start the adventure!
-tavern();
+fighting_postion_Kilo();
 cout << "\n=== THE END ===" << endl;
 return 0;
 }
 // ========== LOCATION FUNCTIONS ==========
 // Each function is one "node" in your story
-void tavern()
+void fighting_postion_Kilo()
 {
-cout << "\nYou wake up in a dusty tavern." << endl;
-cout << "The bartender says there's trouble in the kingdom." << endl;
-cout << "Will you help? (1 = Yes, 2 = No): ";
+cout << "\nYour vison slowly returns to you and you hear lasgun fire and explosions all around you. Your soldiers are screaming and fighting to hold the enemy back. You hear your commander hailing you on your vox. "  << endl;
+cout << "Lt Smith, gather your forces and fallback to the city center to secure the Governor." << endl;
+cout << "Will you fallback? (1 = Yes, 2 = No): ";
 int choice;
 cin >> choice;
 if (choice == 1)
 {
-cout << "\n'Brave soul! Head to the forest!'" << endl;
+cout << "\n'You gather your forces and lead a fighting withdraw back to the city center. During the withdraw your forces are harassed by enemy jet bikes and aircraft trying to break up your formation. But you keep your forces tougher and repel each enemy attack and make it to the city center to rejoin what remains of the planetary defense forces. !'" << endl;
 forest(); // Go to forest function
 }
 else
 {
-cout << "\n'Coward! Get out of my tavern!'" << endl;
+cout << "\n'The fighting intensifies and you organize your soldiers into a formidable defense but your heavy bolters run out of ammo and a sudden high pitched screech rings out and a flash of light speeds though the air and hits your chimera destroying it. The enemy quickly overcomes your forces and the planet falls.  !'" << endl;
 gameOver(); // Go to game over
 }
 }
