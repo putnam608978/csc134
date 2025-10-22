871f0d3e663ffc5e27bf315047754cf7e1c6ba4e
putnam608978 <putnam.elijah4031@gmail.com>
2025-10-22 14:58:51 +0000

    added more code

--- m5t1.cpp
+++ m5t1.cpp
@@ -14,21 +14,38 @@ void say_hello();
 
 int get_the_answer();
 
-double double_a_number();
+double double_a_number(double);
 
 int main(){
 
         double my_num;
         int another_num;
-
+    say_hello();
     cout << "Welcome to the best program ever!" << endl;
     cout << "Please enter a number (with or without decimal place)." << endl;
     cin >> my_num;
     my_num = my_num * 2;
     cout << "Double the number is: " << my_num << endl;
+    cout << "But the only answer you need is: ";
+    cout << get_the_answer() << endl;
 
 }
 
 
+// function definitions 
+void say_hello(){
+    // says hi
+    cout << "Welcome to the best program ever!" << endl;
+}
 
+int get_the_answer(){
+    // provides the answer to everything
+    return 42;
+}
 
+double double_a_number(double the_num){
+// num times two
+double answer = the_num * 2;
+return answer;
+
+}
