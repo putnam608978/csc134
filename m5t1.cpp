0699bab85aacce8c7eb0a29ef6db44d3bca95883
putnam608978 <putnam.elijah4031@gmail.com>
2025-10-22 14:45:15 +0000

    started code tested worked with number 5

---
+++ m5t1.cpp
@@ -0,0 +1,34 @@
+/*
+csc 134
+M5T1 - Basic Functions
+Putnam, Elijah
+10/22/25
+*/
+
+
+#include <iostream>
+using namespace std;
+
+// Function Declarations
+void say_hello();
+
+int get_the_answer();
+
+double double_a_number();
+
+int main(){
+
+        double my_num;
+        int another_num;
+
+    cout << "Welcome to the best program ever!" << endl;
+    cout << "Please enter a number (with or without decimal place)." << endl;
+    cin >> my_num;
+    my_num = my_num * 2;
+    cout << "Double the number is: " << my_num << endl;
+
+}
+
+
+
+
