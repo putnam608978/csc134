a93c9a4413ab3a0ca80016ed4566288fdbd1d9e3
putnam608978 <putnam.elijah4031@gmail.com>
2025-11-03 15:34:33 +0000

    save

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -4,7 +4,6 @@ Putnam, Elijah
 11/3/2025
 */
 
-
 #include <iostream>
 using namespace std;
 
@@ -12,6 +11,12 @@ using namespace std;
 // the getLength,
 // getWidth, getArea, and displayData
 // functions here.
+// The Declaration "promises" these functions will exist if called.
+
+double getLength(); // get the length
+double getWidth();  // get the width
+double getArea(double length, double width);   // *calculate* the area 
+void   displayData(double length, double width, double area); // display the output
 
 int main()
 {
@@ -29,7 +34,7 @@ int main()
    width = getWidth()
    
    // Get the rectangle's area.
-   are = getArea();
+   area = getArea();
    
    // Display the rectangle's data.
    displayData(length, width, area);
@@ -37,3 +42,32 @@ int main()
    return 0;
 }
 
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//********************************
+// Remember, the Definition is fulfilling the "promise" of the Declarations.
+
+//getLength - Asks the user to enter a rectangle's length, and return that value as a double.
+double getLength() {
+    cout << "Not implemented." << endl;
+    return 0.0;
+}
+
+
+//getWidth - Asks the user to enter a rectangle's width, and return that value as a double.
+double getWidth() {
+    cout << "Not implemented." << endl;
+    return 0.0; 
+}
+
+//getArea - This function should take two arguments, length and width. It will calculate the area and return that value as a double.
+double getArea(double length, double width) {
+    cout << "Not implemented." << endl;
+    return 0.0;
+}
+
+// displayData - Void function, simply outputs the values in a readable format.
+void   displayData(double length, double width, double area) {
+    cout << "TODO: Display the data" << endl;
+}
