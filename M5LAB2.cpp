afdcd6302d81991780cbeb01812a57b7a4f1e199
putnam608978 <putnam.elijah4031@gmail.com>
2025-11-03 15:17:12 +0000

    brought in bobs half finished code

--- M5LAB2.cpp
+++ M5LAB2.cpp
@@ -1,9 +1,39 @@
 /*
-M5LAB2
+M5LAB2- completeing Bobs half finished program
 Putnam, Elijah 
 11/3/2025
 */
 
 
+#include <iostream>
+using namespace std;
 
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
 
