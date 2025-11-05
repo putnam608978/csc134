353a7378a9e3bf4134c962da69b9ba288364953b
putnam608978 <putnam.elijah4031@gmail.com>
2025-11-05 15:32:52 +0000

    save 1

--- M5LAB1.cpp
+++ M5LAB1.cpp
@@ -3,95 +3,46 @@ Putnam,Elijah
 11/5/2025
 */
 
-
 #include <iostream>
 using namespace std;
 
-/*
-CSC 134
-M5LAB1 - Choose Your Own Adventure
-[name]
-[date]
-This template serves as a starting point
-for your own program. We'll be using
-void functions to tell a simple story,
-where the user gets to make choices.
+// Function declarations
+int getPlayerChoice(int maxChoice); // let player choose options
 
-You can fork this template by clicking on the name above, then click the three
-dots on the top right, and click 'fork repl'. This makes a copy for you to use.
-*/
+// main()
+void main(){
+    int choice;
+    int max = 3;
+    cout << "TESTING: CHOOSE 1, 2, OR 3. " << endl;
+    choice = getPlayerChoice(max);
+    cout << "You chose: " << choice << endl;
 
-// Function Prototypes
-// Any functions you make will need to
-// be listed here, in this format, at the top.
-// This is so the compiler knows to save
-// room for them in the final program.
-void main_menu();
-void choice_front_door();
-void choice_back_door();
-void choice_go_home();
-// TODO: add more choices here
-
-int main() {
-  cout << "M5LAB1 - Choose Your Own Adventure" << endl;
-  // load up the main menu
-  main_menu();
-  // when we return here, we're done
-  cout << "Thanks for playing!" << endl;
-  return 0; // finished with no errors
+    //ending
+    return 0;
 }
+// ============================================================================
+// GAME LOGIC FUNCTIONS
+// ============================================================================
+// These functions handle the mechanics of running the game.
 
-void main_menu() {
-  // Write a simple menu that lets the user choose 1,2, or 3, or 4 to quit.
-  cout << "Main Menu" << endl;
-  cout << "You're in front of a spooky old house..." << endl;
-  cout << "Do you:" << endl;
-  cout << "1. Try the front door" << endl;
-  cout << "2. Sneak around back" << endl;
-  cout << "3. Forget it, and go home" << endl;
-  cout << "4. [Quit]" << endl;
-  cout << "Choose: ";
-  int choice;
-  cin >> choice;
-  if (1 == choice) {
-    choice_front_door();
-  } else if (2 == choice) {
-    // call choice 2 here
-  } else if (3 == choice) {
-    // call choice 3 here
-  } else if (4 == choice) {
-    cout << "Ok, quitting game" << endl;
-    return; // go back to main()
-  } else {
-    cout << "That's not a valid choice, please try again." << endl;
-    cin.ignore(); // clear the user input
-    main_menu();  // try again
-  }
+/**
+ * Get a valid choice from the player.
+ *
+ * @param maxChoice Highest valid choice number
+ * @return Zero-based index of the chosen option
+ *
+ */
+int getPlayerChoice(int maxChoice) {
+    int choice;
+    while (true) {
+        cout << "Your choice: ";
+        cin >> choice;
+
+        // Validate range (remember: player sees 1-N, we need 0-(N-1))
+        if (choice >= 1 && choice <= maxChoice) {
+            return choice;
+        }
+
+        cout << "Please choose between 1 and " << maxChoice << ".\n";
+    }
 }
-
-// FUNCTION DEFINITIONS
-// OK, we have the prototypes at the top, but
-// now we have to actually write the functions.
-// They go here, after main().
-void choice_front_door() {
-  cout << "Try the front door." << endl;
-  cout << "It's locked. " << endl;
-  cout << "Do you:" << endl;
-  cout << "1. Check around back" << endl;
-  cout << "2. Give up and go home" << endl;
-  int choice;
-  cout << "Choose: ";
-  cin >> choice;
-  if (1 == choice) {
-    choice_back_door();
-  } else if (2 == choice) {
-    choice_go_home();
-  }
-}
-
-void choice_back_door() { cout << "TODO: Write something here" << endl; }
-
-void choice_go_home() { cout << "TODO: Write something here" << endl; }
-
-// any new choices go here
-
