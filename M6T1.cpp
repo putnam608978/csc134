f926062d8fd166b897e71d258cded5074d74fd31
putnam608978 <putnam.elijah4031@gmail.com>
2025-11-10 15:49:42 +0000

    save 1

--- M6T1.cpp
+++ M6T1.cpp
@@ -8,7 +8,42 @@ Putnam,Elijah
 #include <iostream>
 using namespace std;
 
+// Function Declarations
+void method1();
+void method2();
+
+// main
 int main() {
+    // Count # of Pokemon per day, two different ways
+    method1();
+    //method2();
+
+    return 0;
+}
+
+// function definitions
+void method1() {
+    // Method one - no arrays
+    // Count 5 days Pokemon, get total and average
+    cout << "Enter each Pokemon found per day." << endl;
+    cout << "Day 0 = Monday, Day 4 = Friday" << endl;
+    const int SIZE = 5;
+    int count = 0;
+    int poke_today;     // current value, to add
+    int poke_total = 0; // add up 
+    double poke_avg = 0;// average
+
+    while (count < SIZE) {
+        cout << "Day " << count << ": ";
+        cin >> poke_today;
+        poke_total += poke_today;
+        count++; // Move to next day
+    }
+    cout << "Total = " << poke_total << endl;
+}
+
+void method2() {
+    // Method 2 - with arrays
     const int SIZE = 3;
     int temps[SIZE];
 
@@ -34,7 +69,5 @@ int main() {
         cout << "Temp #" << count << " = " << temps[count] << endl;
         count++; // move to next
     } while (count < SIZE);
-
-    return 0;
 }
 
